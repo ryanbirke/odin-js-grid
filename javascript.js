a332c7120e923b8d8ce1546da280c3e575c73316
Ryan Birke <8440910+ryanbirke@users.noreply.github.com>
2024-11-25 21:11:11 -0500

    Add function to create 10 divs

--- javascript.js
+++ javascript.js
@@ -1,1 +1,12 @@
- 
+function createGrid(size = 10) {
+    const containerDiv = document.querySelector(".gridContainer");
+
+    for(let i = 0; i < size; i++) {
+        const newBox = document.createElement("div");
+        newBox.setAttribute("class", "gridBox");
+        containerDiv.appendChild(newBox);
+        console.log(`Created new box ${(i+1)}`)
+    }
+}
+
+createGrid();
