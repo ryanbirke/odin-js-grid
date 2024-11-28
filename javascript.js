2cc130f73edb887b3beb6686304cf4b4b09022b0
Ryan Birke <8440910+ryanbirke@users.noreply.github.com>
2024-11-28 14:28:05 -0500

    Create 4x4 dummy grid

--- javascript.js
+++ javascript.js
@@ -1,12 +1,24 @@
-function createGrid(size = 10) {
+function createGrid(size = 16) {
     const containerDiv = document.querySelector(".gridContainer");
 
     for(let i = 0; i < size; i++) {
         const newBox = document.createElement("div");
         newBox.setAttribute("class", "gridBox");
+        newBox.setAttribute("style", gridStyle);
         containerDiv.appendChild(newBox);
         console.log(`Created new box ${(i+1)}`)
     }
 }
 
+const gridStyle = 
+    `border-color: #000000; 
+    border-width: 1px;
+    border-style: solid;
+    box-sizing: border-box;
+    margin: 2px;
+    width: 40px;
+    height: 40px;
+    flex: auto`;
+
+    
 createGrid();
